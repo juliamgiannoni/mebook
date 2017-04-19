d059d43642277b1d4f8dfe3e415471a664b901fb
Julia Yi <juliamijiyi@gmail.com>
2017-04-19 12:31:16 -0700

    Updated New User route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
 
-  def create
+  def index
+    @users = User.all
+  end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def edit
