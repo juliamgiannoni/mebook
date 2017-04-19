6162d80182fa1f5d9f88d8bbf0df665cec604316
Julia Yi <juliamijiyi@gmail.com>
2017-04-19 15:00:31 -0700

    Updates routes and posts index

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :users
 
-  root to: 'users#index'
 
-  get 'users', to: 'users#index'
+  root to: 'posts#index'
 
   get 'users/new', to: 'users#new'
 
   get 'users/:id', to: 'users#show', as: :user
 
-
   post 'users', to: 'users#create'
 
-  # get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
 
-  resources :posts
+  get 'posts/:id', to: 'posts#show', as: :post
+
+  post 'posts', to: 'posts#create'
+
 
 
 end
