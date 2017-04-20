edb8216aaefcb504badee0187c1cc7e69355b658
Julia Yi <juliamijiyi@gmail.com>
2017-04-19 20:14:44 -0700

    Finalized mebook

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,15 +9,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @user = User.all
-    if User.exists?
       @post = Post.create(post_params)
       if @post.save
         redirect_to root_path
       else
         render :new
       end
-    end
 
   end
 
